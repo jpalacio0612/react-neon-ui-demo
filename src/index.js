76690c3f72d38cb73912ad4d6447d5223da56c76
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-25 22:51:19 -0500

    support for Sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import {Hello} from './components/Hello'
+import './app.scss'
 
 ReactDom.render(<Hello/>,document.getElementById("app"))
