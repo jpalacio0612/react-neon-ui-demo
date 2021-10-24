7e788c3f18dc31026cfa2545e2d1798beab343e1
jpalacio0612 <jpalacio0612@gmail.com>
2021-10-24 09:00:18 -0500

    fix typos

--- src/components/GettingStarted/index.js
+++ src/components/GettingStarted/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Header } from '../Header'
 
 import SyntaxHighlighter from 'react-syntax-highlighter'
@@ -12,25 +12,25 @@ export const GettingStarted = () => {
         <h1> How to install </h1>
         <p>For npm users</p>
         <SyntaxHighlighter language='jsx' style={monokai}>
-          {`$ npm install react-neon-ui styled-components numeral`}
+          {`$ npm install react-neon-ui styled-components`}
         </SyntaxHighlighter>
         <p>For yarn users</p>
         <SyntaxHighlighter language='jsx' style={monokai}>
-          {`$ yarn add react-neon-ui styled-components numeral`}
+          {`$ yarn add react-neon-ui styled-components`}
         </SyntaxHighlighter>
         <h1> Example </h1>
         <SyntaxHighlighter language='jsx' style={monokai}>
           {`
-          import React from 'react'
-          import { Buttom } from 'react-neon-ui'
+  import React from 'react'
+  import { Button } from 'react-neon-ui'
 
-          const App = () => (
-            <div>
-              <Button>Click Here</Button>
-            </div>
-          )
+  const App = () => (
+    <div>
+      <Button>Click Here</Button>
+    </div>
+  )
 
-          export default App
+  export default App
           `}
         </SyntaxHighlighter>
       </div>
